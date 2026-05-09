55f7b7320162fe0bde6ef4718b463fe87d424610
RS™ <sharky.owlskv@gmail.com>
2026-05-09 11:29:32 +0000

    new

--- client/src/pages/modcp.tsx
+++ client/src/pages/modcp.tsx
@@ -10,7 +10,7 @@ import { Badge } from "@/components/ui/badge";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Skeleton } from "@/components/ui/skeleton";
-import img1svg from "img1.svg"
+import img1svg from "@/img1.svg"
 import {
   Select,
   SelectContent,
